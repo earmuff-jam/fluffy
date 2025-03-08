408ab5aa11a1dd38b6ea0a7364fce1aeefac8fdb
mohit <mohit.paudyal@gmail.com>
2025-03-08 17:11:03 -0600

    Added custom landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,36 @@
-import { lightTheme } from '@utils/Theme';
 import * as ReactDOM from 'react-dom/client';
+
+import { lightTheme } from '@utils/Theme';
+
 import { SnackbarProvider } from 'notistack';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import ApplicationValidator from '@src/ApplicationValidator';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { CssBaseline, ThemeProvider, Button, Typography } from '@mui/material';
+
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { Authenticator, useAuthenticator, View } from '@aws-amplify/ui-react';
+import ApplicationValidator from '@src/ApplicationValidator';
+
+import AuthFooter from '@features/LandingPage/AuthFooter';
+import AuthHeader from '@features/LandingPage/AuthHeader';
+
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 Amplify.configure(outputs);
 const queryClient = new QueryClient();
 
+const components = {
+  Header() {
+    return <AuthHeader />;
+  },
+  Footer() {
+    return <AuthFooter />;
+  },
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Authenticator>
+  <Authenticator components={components}>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
