8335914371cf7c8545381398bc719aa6fb6fcd9e
mohit <mohit.paudyal@gmail.com>
2025-03-17 16:31:19 -0500

    Refactor for storybook 8 upgrade
    - Updated .storybook files for version 8 upgrade
    - Removed un-used deps in package.json to make app size small
    - Updated vitest.workspace.ts file for vitest related stuffs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,37 +1,15 @@
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-    '@storybook/addon-themes',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {
-      builder: {
-        useSWC: true,
-      },
-    },
-  },
-  staticDirs: ['../public'],
-  webpackFinal: async (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@features': path.resolve(__dirname, '../src/features'),
-      '@common': path.resolve(__dirname, '../src/common'),
-      '@utils': path.resolve(__dirname, '../src/utils'),
-      '@services': path.resolve(__dirname, '../src/services'),
-    };
-    return config;
+    name: '@storybook/react-vite',
+    options: {},
   },
 };
 export default config;
